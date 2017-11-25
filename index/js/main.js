9544e3a0043335cb6f3b061bfc407a2008c3c962
李月 <leeue@foxmail.com>
2017-11-25 23:52:58 +0800

    网页案例

--- index/js/main.js
+++ index/js/main.js
@@ -4,7 +4,7 @@
 <!--拖动滚动条或滚动鼠标轮-->
 window.onscroll=function(){
     if(document.body.scrollTop||document.documentElement.scrollTop>500){
-        console.log(document.documentElement.scrollTop);
+     //   console.log(document.documentElement.scrollTop);
         document.getElementById('gomain').style.display="block"
     }else{
         document.getElementById('gomain').style.display="none"
